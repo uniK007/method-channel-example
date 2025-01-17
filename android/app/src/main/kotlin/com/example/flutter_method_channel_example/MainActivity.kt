3db247c59e05e41ff8e89416e5d75cb61f2e1359
uniK007 <kislayk970@gmail.com>
2025-01-17 12:41:58 +0530

    native duplicate imports update

--- android/app/src/main/kotlin/com/example/flutter_method_channel_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_method_channel_example/MainActivity.kt
@@ -7,7 +7,6 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
